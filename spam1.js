880f6137cc7b3c7ed45990aa2640109b8e70339e
wolfachraf5 <47570819+wolfachraf5@users.noreply.github.com>
2019-02-17 22:49:43 +0100

    Add files via upload

---
+++ spam1.js
@@ -0,0 +1,88 @@
+const  Discord  =  تتطلب ( " discord.js " )؛
+const  client  =  new  Discord.Client (꧁ŢĦĒ.༒ĦĀ❥ķĘŖ꧂)؛
+const  client2  =  new  Discord.Client (꧁LØRÃ^_^꧂)؛
+
+العميل . على ( " جاهز " ، () => {
+   وحدة التحكم . سجل ( ` ---------------- ` ) ؛
+   وحدة التحكم . تسجيل ( ` الائتمان Farmm - سيناريو بقلم: Kahrbaa ` )؛
+   وحدة التحكم . سجل ( ` ---------------- ` ) ؛
+   وحدة التحكم . تسجيل ( ` Loadinng ` )؛
+   وحدة التحكم . تسجيل ( ` Loadinng. ` )؛
+   وحدة التحكم . تسجيل ( ` Loadinng .. ` )؛
+   وحدة التحكم . log ( ` Loadinng ... ` )؛
+   وحدة التحكم . log ( ` This Bots Online ' ` )؛
+   وحدة التحكم . سجل ( ` ---------------- ` ) ؛
+})؛
+
+
+العميل . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -راتب ' ) {
+        رسالة . قناة . ارسل ( ' #Daily ' )
+    }
+})؛
+
+العميل . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -مبلغ ' ) {
+        رسالة . قناة . أرسل ( ' #credits ' )
+    }
+})؛
+
+العميل . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -rip ' ) {
+        رسالة . قناة . أرسل ( ' #credits ' )
+    }
+})؛
+
+client2 . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -راتب ' ) {
+        رسالة . قناة . ارسل ( ' #Daily ' )
+    }
+})؛
+
+client2 . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -مبلغ ' ) {
+        رسالة . قناة . أرسل ( ' #credits ' )
+    }
+})؛
+
+client2 . على ( ' message ' ، message  => {
+    if ( message . content  ===  ' -rip ' ) {
+        رسالة . قناة . أرسل ( ' #credits ' )
+    }
+})؛
+
+
+
+العميل . على ( ' message ' ، message  => { // لا تغير شئ عشان ما تخرب الدنيا
+if ( message . content  ===  ' ! spam ' ) {
+      دع العد =  0 ؛
+      اسمحوا ecount =  0 ؛
+      لـ ( let x =  0 ؛ x <  90000 ؛ x ++ ) {
+        رسالة . قناة . أرسل ( `* Credit Spam، Frame، Credit By Kahrba، Farm Credit By Kahrba، ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح ج ج ** [" $ { x } "] ` )
+          . ثم ( م  => {
+            عد ++ .
+          })
+          
+        }
+      }
+})؛
+
+client2 . على ( ' message ' ، message  => { // لا تغير شئ عشان ما تخرب الدنيا
+if ( message . content  ===  ' ! spam ' ) {
+      دع العد =  0 ؛
+      اسمحوا ecount =  0 ؛
+      لـ ( let x =  0 ؛ x <  90000 ؛ x ++ ) {
+        رسالة . قناة . أرسل ( `* Credit Spam، Frame، Credit By Kahrba، Farm Credit By Kahrba، ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح ج ج ** [" $ { x } "] ` )
+          . ثم ( م  => {
+            عد ++ .
+          })
+          
+        }
+      }
+})؛
+
+
+
+
+العميل . login ("NTI3OTI1MTU5NTQ2NDU0MDI2.D0tcEQ.IFoksTNJw1uP7Td-M8sBRIsBNQA")؛ // لا تغير فيها شيء
+client2 . login ("NTI3OTI1MTU5NTQ2NDU0MDI2.D0tcEQ.IFoksTNJw1uP7Td-M8sBRIsBNQA")؛ // لا تغير فيها شيء
